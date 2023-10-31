2d263dca163a93d1ca8c60cec59010bdad7191d2
DustinYang <2211012@iisigroup.com>
2023-10-31 14:00:08 +0800

    【專案初始化】

---
+++ Web_Small_Function_For_Core7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Small_Function_For_Core7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
